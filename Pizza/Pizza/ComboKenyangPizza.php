dcf7cb8d21ae0916b50d9df6f4ff579ecbb5a453
Syafiqjos <syafiqjos@gmail.com>
2021-11-08 15:02:18 +0700

    Add ComboKenyangPizza class

---
+++ Pizza/Pizza/ComboKenyangPizza.php
@@ -0,0 +1,42 @@
+<?php
+    include_once('./IPizza.php');
+    include_once('./PizzaTopping.php');
+
+    class ComboKenyangPizza implements IPizza {
+        public $ownerName = ""; // owner name
+        public $toppings = array(); // array of toppings
+
+        public function __construct($ownerName)
+        {
+            $this->$ownerName = $ownerName;
+
+            $this->AddTopping(new BeefTopping("Sapi Halal"));
+            $this->AddTopping(new MushroomTopping("Jamur Kemantu Special"));
+            $this->AddTopping(new CheeseTopping("Alerun Special"));
+        }
+
+        public function AddTopping($topping)
+        {
+            array_push($this->toppings, $topping);
+        }
+        
+        public function Prepare()
+        {
+            for ($i = 0; $i < count($this->toppings); $i++) {
+                $this->toppings[$i]->PrepareTopping();
+            }
+        }
+
+        public function Serve() {
+            $content = "Italian Pizza for " . $this->ownerName . " with ";
+            for ($i = 0; $i < count($this->toppings) - 1; $i++) {
+                $content .= $this->toppings[$i]->brand . " ";
+            }
+            $content .= $this->toppings[count($this->toppings) - 1]->brand;
+            $content .= " ready to serve!\n";
+
+            echo ($content);
+        }
+
+    }
+?>
